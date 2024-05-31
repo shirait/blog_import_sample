acfcf1fabcba14907d526d4f72b1323f1a992c07
shirait <hamburgkarry@gmail.com>
2024-05-31 23:42:09 +0900

    ActiveRecord::Relationを返さないscopeをクラスメソッドに変更。 #9

--- app/models/blog_categorization.rb
+++ app/models/blog_categorization.rb
@@ -2,12 +2,12 @@ class BlogCategorization < ApplicationRecord
   belongs_to :blog
   belongs_to :category
 
-  scope :category_selectbox_attributes_for_blogs_index, -> {
+  def self.category_selectbox_attributes_for_blogs_index
     categories_name_count_id = BlogCategorization.joins(:category).group('blog_categorizations.category_id').
       pluck('categories.name', 'count(blog_categorizations.category_id)', 'blog_categorizations.category_id')
 
     categories_name_count_id.map do |name, count, id|
       ["#{name}(件数:#{count})", id]
     end
-  }
+  end
 end
